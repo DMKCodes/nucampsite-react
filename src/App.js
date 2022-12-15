b306cf6400bc5fe40936369c2d47547f472058e6
Code <dmkcodes@gmail.com>
2022-12-15 14:12:07 -0500

    week 3 updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
-  return (
-    <div className="App">
-      hello nucamp!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />}/>
+                <Route path='directory' element={<CampsitesDirectoryPage />}/>
+                <Route path='about' element={<AboutPage />} />
+                <Route path='contact' element={<ContactPage />}/>
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
